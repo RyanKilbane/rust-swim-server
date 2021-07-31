7f5fa40c1cae8530cd1590fb79eb91b3d3558dc3
Your Name <ryankilbane92@hotmail.com>
2021-08-01 00:27:32 +0100

    I think I solved my global state problem with interior mutablility... I think

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,31 @@ mod counter;
 mod process_commands;
 mod parse;
 use std::io::prelude::*;
+use std::cell::RefCell;
 use std::{io::{Read, Error}, net::{TcpListener, TcpStream}};
 use counter::counter::counter::Counter;
 use crate::parse::parse::parse_tokens;
 use crate::process_commands::process_commands::process_commands as process;
 
 fn main() {
-    let global_state: Counter = Counter::new(0);
+    let mut global_state: RefCell<Counter> = RefCell::new(Counter::new(0));
     let listen = TcpListener::bind("127.0.0.1:8080").unwrap();
     for stream in listen.incoming(){
         let stream = stream.unwrap();
         println!("Connection established!");
-        handler(stream, global_state);
+        handler(stream, &global_state);
     }
 }
 
-fn handler(mut stream: TcpStream, counter: Counter){
+fn handler(mut stream: TcpStream, counter: &RefCell<Counter>){
     loop{
         let mut buffer = vec![0; 50];
         stream.read(&mut buffer).unwrap();
-        let response = "HTTP/1.1 200 OK\r\n\r\n";
         let request = String::from_utf8(buffer[..].to_vec()).unwrap();
         println!("{:?}", request.as_bytes());
         let commands = process::process_commands(request);
-        let value = parse_tokens(&commands, &counter);
-        stream.write(response.as_bytes()).unwrap();
+        let value = parse_tokens(&commands, counter).unwrap();
+        stream.write(value.as_bytes()).unwrap();
         stream.flush().unwrap();
     }
     
--- src/parse.rs
+++ src/parse.rs
@@ -2,18 +2,36 @@ pub mod parse{
     use std::format;
     use crate::token::token::token::*;
     use crate::counter::counter::counter::Counter;
+    use std::cell::RefCell;
 
-    pub fn parse_tokens(commands: &Vec<Token>, counter: &Counter) -> Option<String>{
+    pub fn parse_tokens(commands: &Vec<Token>, counter: &RefCell<Counter>) -> Option<String>{
         // Token array should look something like [{, COMMAND, COLON, ACTION, COMMA, VALUE, COLON, INT, }]
-        if commands[2].token == GET{
-            Some(format!("value: {}", counter.state))
+        if commands[3].token == GET{
+            Some(format!("{{value: {}}}", counter.borrow().state))
         }
-        else if commands[2].token == SET {
-            counter.alterState(commands[7].literal.parse::<i64>().unwrap());
-            Some(format!("value: {}", counter.state))
+        else if commands[3].token == SET {
+            counter.borrow_mut().alterState(commands[7].literal.parse::<i64>().unwrap());
+            Some(format!("{{value: {}}}", counter.borrow().state))
         }
         else{
             None
+        }
+    }
+
+    #[cfg(test)]
+    mod test{
+        use std::process;
+
+        use crate::{lexer::lexer::lexer::Lexer, process_commands::process_commands::process_commands::process_commands};
+
+        use super::*;
+        #[test]
+        fn test_parse(){
+            let counter: RefCell<Counter> = RefCell::new(Counter::new(10));
+            let command = "{command:get}";
+            let commands = process_commands(String::from(command));
+            let x = parse_tokens(&commands, &counter);
+            assert_eq!(x.unwrap(), "{value: 10}");
         }
     }
 }
