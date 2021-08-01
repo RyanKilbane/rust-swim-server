78552a5b56afdbf8d6d7e2ff6797aecb8cf34791
Your Name <ryankilbane92@hotmail.com>
2021-08-01 03:58:26 +0100

    I think I'm making progress, I've now gotten an 'already borrowed' error, which I think is coming from using IM on line 30 in main and then again on line 37 in parser

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod counter;
 mod process_commands;
 mod parse;
 mod subscribe;
-use std::borrow::Borrow;
+use std::rc::Rc;
 use std::io::prelude::*;
 use std::cell::RefCell;
 use std::{io::{Read}, net::{TcpListener, TcpStream}};
@@ -15,25 +15,25 @@ use crate::process_commands::process_commands::process_commands as process;
 
 fn main() {
     let mut global_state: RefCell<Counter> = RefCell::new(Counter::new(0));
-    let mut subscribers: RefCell<Subscribers> = RefCell::new(Subscribers::new());
+    let mut subscribers: Rc<RefCell<Subscribers>> = Rc::new(RefCell::new(Subscribers::new()));
     let listen = TcpListener::bind("127.0.0.1:8080").unwrap();
     for stream in listen.incoming(){
-        let stream: TcpStream = stream.unwrap();
+        let stream: Rc<RefCell<TcpStream>> = Rc::new(RefCell::new(stream.unwrap()));
         println!("Connection established!");
-        handler(stream, &global_state, &subscribers);
+        handler(&stream, &global_state, &subscribers);
     }
 }
 
-fn handler(mut stream: TcpStream, counter: &RefCell<Counter>, sub: &RefCell<Subscribers>){
+fn handler(mut stream: &Rc<RefCell<TcpStream>>, counter: &RefCell<Counter>, sub: &Rc<RefCell<Subscribers>>){
     loop{
         let mut buffer = vec![0; 50];
-        stream.read(&mut buffer).unwrap();
+        let mut s = stream.borrow_mut();
+        s.read(&mut buffer).unwrap();
         let request = String::from_utf8(buffer[..].to_vec()).unwrap();
-        println!("{:?}", request.as_bytes());
         let commands = process::process_commands(request);
-        let value = parse_tokens(&commands, counter, sub).unwrap();
-        stream.write(value.as_bytes()).unwrap();
-        stream.flush().unwrap();
+        let value = parse_tokens(&commands, counter, &sub, stream).unwrap();
+        s.write(value.as_bytes()).unwrap();
+        s.flush().unwrap();
     }
     
 }
--- src/parse.rs
+++ src/parse.rs
@@ -3,12 +3,13 @@ pub mod parse{
     use std::io::Read;
     use std::{format, str};
     use std::net::TcpStream;
+    use std::rc::Rc;
     use crate::subscribe::subscribe::subscribe::Subscribers;
     use crate::token::token::token::*;
     use crate::counter::counter::counter::Counter;
     use std::cell::RefCell;
 
-    pub fn parse_tokens(commands: &Vec<Token>, counter: &RefCell<Counter>, subs: &RefCell<Subscribers>) -> Option<String>{
+    pub fn parse_tokens<'a>(commands: &Vec<Token>, counter: &RefCell<Counter>, subs: &Rc<RefCell<Subscribers>>, stream: &Rc<RefCell<TcpStream>>) -> Option<String>{
         // Token array should look something like [{, COMMAND, COLON, ACTION, COMMA, VALUE, COLON, INT, }]
         if commands[3].token == GET{
             Some(format!("{{value: {}}}", counter.borrow().state))
@@ -16,12 +17,14 @@ pub mod parse{
         else if commands[3].token == SET {
             counter.borrow_mut().alter_state(commands[7].literal.parse::<i64>().unwrap());
             let message = format!("{{value: {}}}", counter.borrow().state);
-            // subs.clone().send_message(&message);
+            let refs = subs.borrow_mut();
+            //refs.send_message(&message);
             Some(message)
         }
         else{
-            // let x = stream.borrow();
-            //subs.borrow_mut().add_subscriber(x.try_clone().unwrap());
+            let mut refs = subs.borrow_mut();
+            let s = stream.borrow_mut();
+            refs.add_subscriber(s.try_clone().unwrap());
             None
         }
     }
@@ -39,8 +42,8 @@ pub mod parse{
             let counter: RefCell<Counter> = RefCell::new(Counter::new(10));
             let command = "{command:get}";
             let commands = process_commands(String::from(command));
-            let x = parse_tokens(&commands, &counter, &subs);
-            assert_eq!(x.unwrap(), "{value: 10}");
+            // let x = parse_tokens(&commands, &counter, &subs);
+            // assert_eq!(x.unwrap(), "{value: 10}");
         }
     }
 }
